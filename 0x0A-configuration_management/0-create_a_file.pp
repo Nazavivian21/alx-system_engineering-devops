907f5c11189511a4e04790248a8196544ca20b86
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-29 09:45:49 +0100

    A puppet manifest that creates a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This Puppet manifest creates a file in /tmp/school with specific properties
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
