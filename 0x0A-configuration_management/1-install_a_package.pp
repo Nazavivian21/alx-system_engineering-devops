df2d839df7cf6f9329f718e373e0697e431cf857
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-29 10:37:39 +0100

    Installs a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install Flask version 2.1.0 using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Ensure that pip3 is available
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 and Werkzeug version 2.0.3 using pip3
+exec { '1-install_a_package':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
