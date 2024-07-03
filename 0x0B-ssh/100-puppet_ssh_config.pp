cb2b46162fb42167157c961609bd821192103260
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-07-03 11:26:37 +0100

    Make corrections to the file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,16 @@
-#!/usr/bin/bash
-#Using puppet to connect to my server without password
-file {'/etc/ssh/ssh_config':
+# Using Puppet to connect to my server without a password
+file { '/etc/ssh/ssh_config':
   ensure => 'present',
 }
-file_line {'Turn off passwd auth':
-    path => '/etc/ssh/ssh_config',
-    line => 'PasswordAuthentication no',
-    match => '^#PasswordAuthentication',
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#PasswordAuthentication',
 }
-file_line {'Use_an_Identity_file':
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentifyFile ~/.ssh/school',
-    match => '^#IdentifyFile',
+
+file_line { 'Use an Identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#IdentityFile',
 }
