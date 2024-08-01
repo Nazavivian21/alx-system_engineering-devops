96f2327008ba4441440a49500be5655f4d28417f
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-01 15:24:29 +0100

    Fix a bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,47 +4,48 @@
 This module retrieves data from a REST API
 """
 
-import requests
-import sys
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-# get the user id
-user_id = sys.argv[1]
-# users
-users_url = 'https://jsonplaceholder.typicode.com/users'
+    # get the user id
+    user_id = argv[1]
+    # users
+    users_url = 'https://jsonplaceholder.typicode.com/users'
 
-# todos
-todos_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos/'
+    # todos
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos/'
 
-users_response = requests.get(users_url)
-todos_response = requests.get(todos_url)
+    users_response = requests.get(users_url)
+    todos_response = requests.get(todos_url)
 
-# Get the name of the person with the matching id
-user_name = None
-users = users_response.json()
+    # Get the name of the person with the matching id
+    user_name = None
+    users = users_response.json()
 
-for user in users:
-    # check if the id of this user matches, and store their name
-    if user['id'] == int(user_id):
-        # store the name
-        user_name = user['name']
+    for user in users:
+        # check if the id of this user matches, and store their name
+        if user['id'] == (user_id):
+            # store the name
+            user_name = user['name']
 
-todo_lists = todos_response.json()  # store all the todos for the user
-total_tasks = len(todo_lists)
+    todo_lists = todos_response.json()  # store all the todos for the user
+    total_tasks = len(todo_lists)
 
-completed_todos = []  # store the completed todos
+    completed_todos = []  # store the completed todos
 
-# look through each todo, checking if it is completed
-for todo_list in todo_lists:
-    if todo_list['completed'] is True:
-        completed_todos.append(todo_list)
+    # look through each todo, checking if it is completed
+    for todo_list in todo_lists:
+        if todo_list['completed'] is True:
+            completed_todos.append(todo_list)
 
-completed_count = len(completed_todos)
+    completed_count = len(completed_todos)
 
-# print message about user info
-print(
-    f'Employee {user_name} is done with tasks\
-        ({completed_count}/{total_tasks}):'
-)
-# print titles
-for todo in completed_todos:
-    print(f"\t {todo['title']}")
+    # print message about user info
+    print(
+        f'Employee {user_name} is done with tasks\
+            ({completed_count}/{total_tasks}):'
+    )
+    # print titles
+    for todo in completed_todos:
+        print(f"\t {todo['title']}")
