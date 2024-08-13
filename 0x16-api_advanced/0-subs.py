62662d25d59579f0cea828c5d967dc633e3489c1
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-13 15:13:10 +0100

    reReturns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'subreddit-subscriber-checker/0.1 by your_username'}
-    
+    headers = {
+        'User-Agent': 'subreddit-subscriber-checker/0.1 by your_username'
+    }
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
@@ -14,6 +17,7 @@ def number_of_subscribers(subreddit):
             return 0
     except requests.RequestException:
         return 0
+
 
 # Example usage
 subreddit = "python"
