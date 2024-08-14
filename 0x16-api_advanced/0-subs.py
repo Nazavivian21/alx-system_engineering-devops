0da0465f597176440fdcf017868819ea56f5443f
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-14 09:33:42 +0100

    Queries subscribers on a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ Queries subscribers on a given subreddit"""
-if __name__ == '__main__':
-    import requests
+import requests
 
-    def number_of_subscribers(subreddit):
-        """Return the number of subscribers on a given subreddit"""
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {
-            "User-Agent": "Linux 0x16 api advanced:v1.0.0 (by /u/nazaVivian)"
-        }
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
-            return 0
-        results = response.json().get("data")
-        return results.get("subscribers:")
-    
+
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Linux 0x16 api advanced:v1.0.0 (by /u/nazaVivian)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers:")
