15eb6791f9761ebf4d3e2e5a96a991644fcfdfc3
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-16 01:13:38 +0100

    Returns a list containing the titles of all hot articles for a given subreddit.

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module contains the function recurse.
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Queries the Reddit API and returns a list containing
+    the titles of all hot articles for a given subreddit.
+    If no results are found for the given subreddit, returns None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Linux 0x16 api advanced:v1.0.0 (by /u/nazaVivian)"}
+    params = {"limit": 100, "after": after}
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            posts = data["data"]["children"]
+            for post in posts:
+                hot_list.append(post["data"]["title"])
+            after = data["data"]["after"]
+            if after is not None:
+                return recurse(subreddit, hot_list, after)
+            return hot_list
+        except (KeyError, ValueError):
+            return None
+    else:
+        return None
