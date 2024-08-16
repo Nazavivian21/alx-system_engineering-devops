b552744d64e43bb86ff597dcc35c4f4661ba3dcb
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-16 01:09:10 +0100

    Prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains the function top_ten.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries Reddit API & prints titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Linux 0x16 api advanced:v1.0.0 (by /u/nazaVivian)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            posts = data["data"]["children"]
+            for post in posts:
+                print(post["data"]["title"])
+        except (KeyError, ValueError):
+            print(None)
+    else:
+        print(None)
